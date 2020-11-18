1a5325faf058f7188ee9781565178f728d0933c7
murad.pateev <mpateev@gmail.com>
2020-11-18 16:26:20 +0100

    initial commit

---
+++ app/Config/Filters.php
@@ -0,0 +1,42 @@
+<?php namespace Config;
+
+use App\Filters\OauthFilter;
+use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Filters\CSRF;
+use CodeIgniter\Filters\DebugToolbar;
+
+class Filters extends BaseConfig
+{
+	// Makes reading things below nicer,
+	// and simpler to change out script that's used.
+	public $aliases = [
+		'csrf'     => CSRF::class,
+		'toolbar'  => DebugToolbar::class,
+		'honeypot' => \CodeIgniter\Filters\Honeypot::class,
+    'oauthfilter' => OauthFilter::class,
+	];
+
+	// Always applied before every request
+	public $globals = [
+		'before' => [
+			//'honeypot'
+			// 'csrf',
+		],
+		'after'  => [
+			'toolbar',
+			//'honeypot'
+		],
+	];
+
+	// Works on all of a particular HTTP method
+	// (GET, POST, etc) as BEFORE filters only
+	//     like: 'post' => ['CSRF', 'throttle'],
+	public $methods = [];
+
+	// List filter aliases and any before/after uri patterns
+	// that they should run on, like:
+	//    'isLoggedIn' => ['before' => ['account/*', 'profiles/*']],
+	public $filters = [
+	  'oauthfilter' => ['before' => ['blog', 'blog/*']]
+  ];
+}
---
+++ app/Libraries/Oauth.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Libraries;
+
+use OAuth2\GrantType\UserCredentials;
+use OAuth2\Server;
+use \OAuth2\Storage\Pdo;
+
+class Oauth {
+  var $server;
+
+  function __construct() {
+    $this->init();
+  }
+
+  public function init()
+  {
+    $dsn = getenv('database.default.DSN');
+    $username = getenv('database.default.username');
+    $password = getenv('database.default.password');
+
+    $storage = new Pdo(['dsn' => $dsn, 'username' => $username, 'password' => $password]);
+    $this->server = new Server($storage);
+    $this->server->addGrantType(new UserCredentials($storage));
+  }
+}
